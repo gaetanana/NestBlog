5f64119a6adbf6a1fc8c32a7ebb1cfac6435d1a8
gaeta <gaetan.gonfiantini@gmail.com>
2025-03-28 20:42:31 +0100

    Prisma mis en place dans nest

--- nestblog-backend/src/app.module.ts
+++ nestblog-backend/src/app.module.ts
@@ -9,9 +9,11 @@ import {
 import { APP_GUARD } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
+    PrismaModule,
     ConfigModule.forRoot(), 
     KeycloakConnectModule.register({
       authServerUrl: 'http://localhost:8080',   
