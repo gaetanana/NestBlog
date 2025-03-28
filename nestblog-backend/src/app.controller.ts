06b54890f99128447dba6d37b6a19bc1db431f6d
gaeta <gaetan.gonfiantini@gmail.com>
2025-03-28 18:39:44 +0100

    Integration keycloak et nestjs

--- nestblog-backend/src/app.controller.ts
+++ nestblog-backend/src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { AuthenticatedUser, Roles, Resource } from 'nest-keycloak-connect';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('protected')
+  @Roles({ roles: ['user'] }) 
+  getProtected(@AuthenticatedUser() user: any) {
+    return {
+      message: 'Access granted',
+      user,
+    };
   }
 }
--- nestblog-backend/src/app.module.ts
+++ nestblog-backend/src/app.module.ts
@@ -1,10 +1,38 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import {
+  KeycloakConnectModule,
+  AuthGuard,
+  ResourceGuard,
+  RoleGuard,
+} from 'nest-keycloak-connect';
+import { APP_GUARD } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(), 
+    KeycloakConnectModule.register({
+      authServerUrl: 'http://localhost:8080',   
+      realm: 'NestBlog',
+      clientId: 'nestblog-backend',
+      secret: 'peCGb3FZtMUm7bU7As0OPbkXNY98r2hT',
+      bearerOnly: true,                         
+      logLevels: ['verbose'],
+    })
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: RoleGuard,
+    },
+  ],
 })
-export class AppModule {}
+export class AppModule { }
